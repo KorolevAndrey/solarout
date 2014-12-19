c6c2f8b32b9f3dd01fc80c6d69269810efe5b7fe
Avetis Zakharyan <avetis.zakharyan@gmail.com>
2014-12-19 21:57:41 +0400

    now it is 3d

--- graphics/core/src/com/solarout/graphics/SoraoutGraphics.java
+++ graphics/core/src/com/solarout/graphics/SoraoutGraphics.java
@@ -4,21 +4,23 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 
+import javax.swing.text.View;
+
 public class SoraoutGraphics extends ApplicationAdapter {
 
-	private SolarStage stage;
-	
+	private Viewer3D viewer;
+
 	@Override
 	public void create () {
-		stage = new SolarStage();
+		viewer = new Viewer3D();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
-		stage.act();
-		stage.draw();
+		viewer.act(Gdx.graphics.getDeltaTime());
+		viewer.draw();
 	}
 }
---
+++ graphics/core/src/com/solarout/graphics/actors/Planet3D.java
@@ -0,0 +1,57 @@
+package com.solarout.graphics.actors;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.VertexAttributes;
+import com.badlogic.gdx.graphics.g3d.Material;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.attributes.TextureAttribute;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.graphics.g3d.*;
+
+/**
+ * Created by CyberJoe on 12/19/2014.
+ */
+public class Planet3D {
+
+    private Vector3 position;
+
+    private float radius;
+
+    private Texture texture;
+
+    private Model model;
+
+    public ModelInstance instance;
+
+    public Planet3D(Texture texture, float radius) {
+        this.texture = texture;
+        this.radius = radius;
+
+        ModelBuilder modelBuilder = new ModelBuilder();
+
+        Material material = new Material();
+        material.set(TextureAttribute.createDiffuse(texture));
+
+        model = modelBuilder.createSphere(radius, radius, radius, 50, 50,
+                material,
+                VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal | VertexAttributes.Usage.TextureCoordinates);
+        instance = new ModelInstance(model);
+
+        instance.transform.setToTranslation(0, 0, 0);
+    }
+
+    public void draw(ModelBatch modelBatch, Environment environment) {
+        modelBatch.render(instance, environment);
+    }
+
+    public void setPosition(float x, float y, float z) {
+        instance.transform.setToTranslation(x, y, z);
+    }
+
+    public Vector3 getPosition() {
+        return position;
+    }
+}
