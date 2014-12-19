bfada6e35f0d47845fa659e02102752fc47392a6
Avetis Zakharyan <avetis.zakharyan@gmail.com>
2014-12-19 18:19:06 +0400

    planet actor create

--- graphics/core/src/com/solarout/graphics/SolarStage.java
+++ graphics/core/src/com/solarout/graphics/SolarStage.java
@@ -4,31 +4,24 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.solarout.graphics.actors.PlanetActor;
 
 /**
  * Created by azakhary on 12/19/2014.
  */
 public class SolarStage extends Stage {
 
-    private ShapeRenderer shapeRenderer;
+    private PlanetActor planet;
 
     public SolarStage() {
-        shapeRenderer = new ShapeRenderer();
+        planet = new PlanetActor(30);
+        planet.setX(getWidth()/2);
+        planet.setY(getHeight() / 2);
+        addActor(planet);
     }
 
     @Override
     public void act(float delta) {
-
-    }
-
-    @Override
-    public void draw() {
-        super.draw();
-
-        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
-        shapeRenderer.setColor(Color.RED);
-        shapeRenderer.circle(getWidth()/2, getHeight()/2, 20);
-        shapeRenderer.end();
 
     }
 }
--- graphics/core/src/com/solarout/graphics/actors/PlanetActor.java
+++ graphics/core/src/com/solarout/graphics/actors/PlanetActor.java
@@ -1,15 +1,42 @@
 package com.solarout.graphics.actors;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-
-import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.solarout.engine.*;
 
 /**
  * Created by azakhary on 12/19/2014.
  */
 public class PlanetActor extends Actor {
 
-    public PlanetActor(SphericSolar ) {
-        Vector3
+    private ShapeRenderer shapeRenderer;
+
+    private float radius;
+
+    public PlanetActor(float radius) {
+        this.radius = radius;
+        init();
+    }
+
+    public PlanetActor(SphericStellarBody bodyDescriptor) {
+        radius = bodyDescriptor.getRadius();
+        init();
+    }
+
+    private void init() {
+        shapeRenderer = new ShapeRenderer();
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        super.draw(batch, parentAlpha);
+
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+        shapeRenderer.setColor(Color.RED);
+        shapeRenderer.circle(getX(), getY(), radius);
+        shapeRenderer.end();
+
     }
 }
