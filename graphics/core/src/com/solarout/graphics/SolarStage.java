dd25087c9378fcbf2b4d4a373ce1172086899cee
Avetis Zakharyan <avetis.zakharyan@gmail.com>
2014-12-19 17:45:05 +0400

    normal gradlized project

---
+++ graphics/core/src/com/solarout/graphics/SolarStage.java
@@ -0,0 +1,13 @@
+package com.solarout.graphics;
+
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+/**
+ * Created by azakhary on 12/19/2014.
+ */
+public class SolarStage extends Stage {
+
+    public SolarStage() {
+
+    }
+}
--- graphics/desktop/src/com/solarout/graphics/desktop/DesktopLauncher.java
+++ graphics/desktop/src/com/solarout/graphics/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.solarout.graphics.SoraoutGraphics;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 800;
+		config.height = 800;
 		new LwjglApplication(new SoraoutGraphics(), config);
 	}
 }
