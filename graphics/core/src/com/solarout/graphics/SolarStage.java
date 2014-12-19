da7e866b95f405e6c0c92e1393dc6bfdad166f5e
Avetis Zakharyan <avetis.zakharyan@gmail.com>
2014-12-19 20:25:00 +0400

    latest planetary graphic changes

--- graphics/core/src/com/solarout/graphics/SolarStage.java
+++ graphics/core/src/com/solarout/graphics/SolarStage.java
@@ -1,13 +1,17 @@
 package com.solarout.graphics;
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.solarout.engine.*;
 import com.solarout.graphics.actors.PlanetActor;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
@@ -18,38 +22,63 @@ public class SolarStage extends Stage {
 
     private UniStarSystem solarSystem;
 
-    private PlanetActor planet;
+    private HashMap<String, PlanetActor> planets = new HashMap<String, PlanetActor>();
+
+    private float meterPerPixel = 1000000000;
 
     public SolarStage() {
-
+        super();
 
         Star star = new Star((float) (6.955F * Math.pow(10, 4)), (float) (1.98855F * Math.pow(10, 26)));
         float solarRadius = (float) (2 * (Math.pow(10, 8)));
 
-        solarSystem = new UniStarSystem(solarRadius, star, 100F);
+        solarSystem = new UniStarSystem(solarRadius, star, 100F*1000000);
         Planet planetData = new Planet((float) (6.7F * Math.pow(10, 2)), (float) (5.9 * Math.pow(10, 20)));
         solarSystem.addStellarBody(
                planetData,
                 new Vector3((float) (1.49F * Math.pow(10, 7)), 0, 0),
                 new Velocity(new Vector3(0, 1, 0.01F), (float) (3.5F * Math.pow(10, 1))), UniStarSystem.RelativeObject.RELATIVE_TO_STAR);
 
-        planet = new PlanetActor(planetData);
-        planet.setX(getWidth()/2);
-        planet.setY(getHeight() / 2);
-        addActor(planet);
-    }
 
-    @Override
-    public void act(float delta) {
         Iterator it = solarSystem.getStellarBodies().entrySet().iterator();
         while (it.hasNext()) {
             Map.Entry pairs = (Map.Entry) it.next();
             String bodyName = (String) pairs.getKey();
             SphericStellarBody solarBody = (SphericStellarBody) pairs.getValue();
 
-            System.out.println((int)(solarBody.getPosition().x/1000000));
-            planet.setX(solarBody.getPosition().x/1000000);
-            planet.setY(solarBody.getPosition().y/1000000);
+            PlanetActor planet = new PlanetActor(30);
+            planets.put(bodyName, planet);
+            addActor(planet);
+        }
+
+
+        PlanetActor planetStar = new PlanetActor(30);
+        planetStar.setX(star.getPosition().x/meterPerPixel);
+        planetStar.setY(star.getPosition().y/meterPerPixel);
+        planets.put("star", planetStar);
+        addActor(planetStar);
+
+        ((OrthographicCamera)getCamera()).position.x = star.getPosition().x/meterPerPixel;
+        ((OrthographicCamera)getCamera()).position.y = star.getPosition().y/meterPerPixel;
+
+    }
+
+    @Override
+    public void act(float delta) {
+        super.act(delta);
+
+        Iterator it = solarSystem.getStellarBodies().entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pairs = (Map.Entry) it.next();
+            String bodyName = (String) pairs.getKey();
+            SphericStellarBody solarBody = (SphericStellarBody) pairs.getValue();
+
+            PlanetActor planet = planets.get(bodyName);
+            planet.setX((int)(solarBody.getPosition().x/meterPerPixel));
+            planet.setY((int)(solarBody.getPosition().y/meterPerPixel));
+
+            //planet.setX(0);
+            //planet.setY(0);
         }
 
         try {
--- graphics/core/src/com/solarout/graphics/actors/PlanetActor.java
+++ graphics/core/src/com/solarout/graphics/actors/PlanetActor.java
@@ -1,42 +1,47 @@
 package com.solarout.graphics.actors;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.utils.TransformDrawable;
 import com.solarout.engine.*;
 
 /**
  * Created by azakhary on 12/19/2014.
  */
-public class PlanetActor extends Actor {
+public class PlanetActor extends Image {
 
     private ShapeRenderer shapeRenderer;
 
     private float radius;
 
     public PlanetActor(float radius) {
+        super(new Texture(Gdx.files.internal("pl1.png")));
         this.radius = radius;
         init();
     }
 
     public PlanetActor(SphericStellarBody bodyDescriptor) {
-        radius = bodyDescriptor.getRadius()/1000000;
+        super(new Texture(Gdx.files.internal("pl1.png")));
+        radius = bodyDescriptor.getRadius()/10;
         init();
     }
 
     private void init() {
-        shapeRenderer = new ShapeRenderer();
+        setScale(0.3f, 0.3f);
+        setOrigin(getWidth()/2, getHeight()/2);
     }
 
-    @Override
-    public void draw(Batch batch, float parentAlpha) {
-        super.draw(batch, parentAlpha);
-
-        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
-        shapeRenderer.setColor(Color.RED);
-        shapeRenderer.circle(getX(), getY(), radius);
-        shapeRenderer.end();
-
+    public void setX(float x) {
+        super.setX(x - getWidth()/2f);
     }
+
+    public void setY(float y) {
+        super.setY(y - getHeight()/2f);
+    }
+
 }
