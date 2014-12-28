b2282773af332e33c6919c56154eee6ee12030f0
Aram Tadevosyan <aramtadevosyan@gmail.com>
2014-12-28 23:32:34 +0400

    bugfix

--- graphics/core/src/com/solarout/graphics/Viewer3D.java
+++ graphics/core/src/com/solarout/graphics/Viewer3D.java
@@ -113,7 +113,7 @@ public class Viewer3D {
 
 //        Star star = new Star(6.958E8, 1.989E30);
         Star star = new Star(6.371E6, 5.972E24);
-        solarSystem = new UniStarSystem(1.496E7, star, 0.1F);
+        solarSystem = new UniStarSystem(1.496E7, star, 0.5F);
 
 //        Planet earth = new Planet(6.371E6, 5.972E24, "earth");
 //        solarSystem.addStellarBody(
@@ -131,7 +131,7 @@ public class Viewer3D {
         Planet iss = new Planet(5.5E4, 7.34767309E10, "iss");
         solarSystem.addStellarBody(
                 iss,
-                new DoubleVector3 (0, 3E6, 0),
+                new DoubleVector3 (0, 1E7, 0),
                 new Velocity(new DoubleVector3(0, 0, 1.0233E3)), moon);
 
         Iterator it = solarSystem.getStellarBodies().entrySet().iterator();
