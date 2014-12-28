0a2b48e7966cf855c5e3a9788d23d34c6e206eb0
Avetis Zakharyan <avetis.zakharyan@gmail.com>
2014-12-28 23:30:19 +0400

    TAB

--- graphics/core/src/com/solarout/graphics/UIStage.java
+++ graphics/core/src/com/solarout/graphics/UIStage.java
@@ -3,6 +3,8 @@ package com.solarout.graphics;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.solarout.engine.SphericStellarBody;
@@ -16,13 +18,21 @@ import java.util.Map;
  */
 public class UIStage extends Stage {
 
+    private Label name;
+
     private Label xPos;
     private Label yPos;
     private Label zPos;
 
     private Label scalar;
+    private Label scalarVec;
+
+    private UniStarSystem solarSystem;
+
+    private int currInfoIndex = 0;
 
     public UIStage() {
+
         FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("arial.ttf"));
         FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
         parameter.size = 12;
@@ -32,9 +42,14 @@ public class UIStage extends Stage {
         Label.LabelStyle textStyle = new Label.LabelStyle();
         textStyle.font = font;
 
+        name = new Label("", textStyle);
+        name.setWidth(100);
+        name.setY(getHeight()-name.getHeight()-10f);
+        addActor(name);
+
         xPos = new Label("", textStyle);
         xPos.setWidth(100);
-        xPos.setY(getHeight()-xPos.getHeight()-10f);
+        xPos.setY(name.getY()-xPos.getHeight()-10f);
         addActor(xPos);
 
         yPos = new Label("", textStyle);
@@ -51,28 +66,53 @@ public class UIStage extends Stage {
         scalar.setWidth(100);
         scalar.setY(zPos.getY()-scalar.getHeight()-10f);
         addActor(scalar);
+
+        scalarVec = new Label("", textStyle);
+        scalarVec.setWidth(100);
+        scalarVec.setY(scalar.getY()-scalarVec.getHeight()-10f);
+        addActor(scalarVec);
+
+        addListener(new InputListener() {
+            public boolean keyUp (InputEvent event, int keycode) {
+                if(keycode == 61) {
+                    currInfoIndex++;
+                    if(currInfoIndex>solarSystem.getStellarBodies().size()-1) {
+                        currInfoIndex = 0;
+                    }
+                }
+                return false;
+            }
+        });
     }
 
-    public void update(UniStarSystem solarSystem) {
+    public void setSystem(UniStarSystem solarSystem) {
+        this.solarSystem = solarSystem;
+    }
+
+    public void update() {
+        if(solarSystem == null) return;
+
         Iterator it = solarSystem.getStellarBodies().entrySet().iterator();
+        int iter = 0;
         while (it.hasNext()) {
             Map.Entry pairs = (Map.Entry) it.next();
             String bodyName = (String) pairs.getKey();
             SphericStellarBody solarBody = (SphericStellarBody) pairs.getValue();
 
-            SphericStellarBody earth = solarSystem.getStellarBodies().get("earth");
-            SphericStellarBody moon = solarSystem.getStellarBodies().get("moon");
-
-            updateLbl(xPos, "X: " + solarBody.getPosition().x);
-            updateLbl(yPos, "Y: " + solarBody.getPosition().y);
-            updateLbl(zPos, "Z: " + solarBody.getPosition().z);
-            updateLbl(scalar, "SCALAR: " + solarBody.getVelocity().getScalar());
-
+            if(iter == currInfoIndex) {
+                updateLbl(name, "Name: " + solarBody.getName());
+                updateLbl(xPos, "X: " + solarBody.getPosition().x);
+                updateLbl(yPos, "Y: " + solarBody.getPosition().y);
+                updateLbl(zPos, "Z: " + solarBody.getPosition().z);
+                updateLbl(scalar, "vel scalar: " + solarBody.getVelocity().getScalar());
+                updateLbl(scalarVec, "vel vector: x:" + solarBody.getVelocity().getVector().x + ", y:" + solarBody.getVelocity().getVector().y + ", z:" + solarBody.getVelocity().getVector().z);
+            }
+            iter++;
         }
     }
 
     private void updateLbl(Label lbl, String value) {
         lbl.setText( value);
-        lbl.setX(getWidth()-lbl.getWidth()-5f);
+        lbl.setX(10f);
     }
 }
